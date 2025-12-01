88cb3062dd711502e1b6098a47b3c527564834c2
Dimitris Marg <97585467+dimitrisftth1@users.noreply.github.com>
2025-12-01 16:35:43 +0200

    Add files via upload

---
+++ automaticConversions.java
@@ -0,0 +1,12 @@
+public class automaticConversions {
+    public static void main(String[] args) {
+        int i;
+        short s=100;
+        i=s;
+        float f=0.01f;
+        double d;
+        d=f;
+        System.out.println(i);
+        System.out.println(d);
+    }
+}
---
+++ real.java
@@ -0,0 +1,8 @@
+public class real {
+    public static void main(String[] args) {
+        double f1, f2;
+        f1 = 15.12;
+        f2 = 132.939;
+        System.out.println(f1*f2);
+    }
+}
